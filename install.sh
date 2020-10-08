11daac95dc90485ecbf027221919a6b120a228af
dot-nuts <72578311+dot-nuts@users.noreply.github.com>
2020-10-09 00:07:37 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
-url=$1
-echo url
+url="$1"
+filename=$(basename "$url")
+workpath=$(dirname $(readlink -f $0))
+curl $url -o $workpath/$filename -s
+sudo dpkg --install  $workpath/$filename
