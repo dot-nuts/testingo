d933737c01a0ba61d94fadb9064f3f503e1bead2
dot-nuts <72578311+dot-nuts@users.noreply.github.com>
2020-10-09 00:01:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,5 @@
 url=$1
 filename=$(basename "$url")
 workpath=$(dirname $(readlink -f $0))
-if (test -f /usr/bin/curl)
-then
-	curl $url -o $workpath/$filename -s
-else
-	if (test -f /usr/bin/wget)
-	then
-		wget -q $url -O $workpath/$filename
-	else
-		echo "No wget or curl installed, installing curl now"
-		sudo apt-get -y install curl
-		curl $url -o $workpath/$filename -s
-	fi
-fi
+curl $url -o $workpath/$filename -s
 sudo dpkg --install  $workpath/$filename
