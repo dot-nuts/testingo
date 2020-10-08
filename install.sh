ed5ddd3f84a5b78171a907158172650fa12cb303
dot-nuts <72578311+dot-nuts@users.noreply.github.com>
2020-10-09 01:56:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 echo "url"
 url="$2"
-echo url
+echo $url
 echo "filename"
 filename=$(basename "$url")
-echo filename
+echo $filename
 echo "workpath"
 workpath=$(dirname $(readlink -f $0))
-echo workpath
+echo $workpath
 echo "curl"
 curl $url -o $workpath/$filename -s
 echo "dpkg"
