135ec6bae5f97619dd9a00e4d596543f5de38bcf
dot-nuts <72578311+dot-nuts@users.noreply.github.com>
2020-10-12 01:37:24 +0300

    Create install_6.sh

---
+++ install_6.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+echo "url"
+url="$6"
+echo $url
+echo "filename"
+filename=$(basename "$url")
+echo $filename
+echo "workpath"
+workpath=$(dirname $(readlink -f $0))
+echo $workpath
+echo "curl"
+curl $url -o $workpath/$filename -s
+echo "dpkg"
+sudo dpkg --install  $workpath/$filename
